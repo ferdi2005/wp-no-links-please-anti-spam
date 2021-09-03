770c609888b6f97349f0983520418aa440ba5405
ferdi2005 <ferdi.traversa@gmail.com>
2021-09-03 23:36:25 +0200

    Only asd, please!

--- wp-no-links-please-anti-spam.php
+++ wp-no-links-please-anti-spam.php
@@ -1,15 +1,15 @@
 <?php
 /*
-Plugin Name: No links please! Anti-SPAM
+Plugin Name: Only asd please! Anti-SPAM!
 Version:     1.3.0
-Description: This simple but effective anti-SPAM system protects your WordPress site from SPAM. It works without imposing annoying CAPTCHAs, quizzes, configurations, third-party services, artificial intelligence or unicorns. How? It just drops any anonymous comment with links inside, alerting humans about this netiquette.
-Author:      Valerio Bozzolan
+Description: This simple but effective anti-SPAM system protects your WordPress site from SPAM. It works without imposing annoying CAPTCHAs, quizzes, configurations, third-party services, artificial intelligence or unicorns. How? It just drops any anonymous comment without any asd inside, alerting humans about this netiquette.
+Author:      Valerio Bozzolan, Ferdinando Traversa
 Author URI:  https://boz.reyboz.it/?l=en
-Plugin URI:  https://github.com/valerio-bozzolan/wp-no-links-please-anti-spam
+Plugin URI:  https://github.com/ferdi2005/wp-only-asd-please-anti-spam
 License:     GPL3+
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 Domain Path: /languages
-Text Domain: no-links-please-anti-spam
+Text Domain: only-asd-please-anti-spam
 */
 
 defined( 'ABSPATH' ) or die( 'Hello lamer!' );
@@ -21,16 +21,16 @@ defined( 'ABSPATH' ) or die( 'Hello lamer!' );
  * @param $commentdata array
  * @since 1.0.0
  */
-function no_links_please_anti_spam_handler( $approved, $commentdata ) {
+function only_asd_please_anti_spam_handler( $approved, $commentdata ) {
 	if( empty( $commentdata[ 'user_ID' ] ) && empty( $commentdata[ 'type' ] ) ) {
-		$found = preg_match( '@(https?://|www\.)[^\",]+@i', $commentdata[ 'comment_content' ] );
-		if( ! empty( $commentdata[ 'comment_author_url' ] ) || $found === 1 ) {
+		$contains_asd = str_contains( $commentdata[ 'comment_content' ], "asd" );
+		if( ! empty( $commentdata[ 'comment_author_url' ] ) || $contains_asd === false ) {
 			// increment counters
-			update_option( 'no_links_please_anti_spam_count', no_links_please_anti_spam_counter() + 1, false );
+			update_option( 'only_asd_please_anti_spam_count', only_asd_please_anti_spam_counter() + 1, false );
 
 			// die with an error message
-			$message = __( "Please try again but without links in the text. Thank you.", 'no-links-please-anti-spam' );
-			$message = apply_filters( 'no_links_please_anti_spam_error', $message );
+			$message = __( "Please try again, adding a sacred asd in your comment. Thank you, asd.", 'only-asd-please-anti-spam' );
+			$message = apply_filters( 'only_asd_please_anti_spam_error', $message );
 			wp_die( $message, $title, [
 				'response'  => 400,
 				'back_link' => true,
@@ -39,52 +39,52 @@ function no_links_please_anti_spam_handler( $approved, $commentdata ) {
 	}
 	return $approved;
 }
-add_filter( 'pre_comment_approved', 'no_links_please_anti_spam_handler', '99', 2 );
+add_filter( 'pre_comment_approved', 'only_asd_please_anti_spam_handler', '99', 2 );
 
 /**
  * Remove the author URL from the comment form for anonymous users
  */
-function no_links_please_anti_spam_form_default_fields( $fields ) {
+function only_asd_please_anti_spam_form_default_fields( $fields ) {
 	if( ! is_user_logged_in() ) {
 		// remove author URL
 		unset( $fields[ 'url' ] );
 
 		// show netiquette message
-		$netiquette = __( "Please remember that links are not appreciated inside comments.", 'no-links-please-anti-spam' );
-		$netiquette = apply_filters( 'no_links_please_anti_spam_netiquette', $netiquette );
-		$fields[ 'comment_form_before' ] .= "<p class=\"no-links-please-anti-spam-netiquette\">$netiquette</p>";
+		$netiquette = __( "Please remember that comments without an asd inside are not appreciated, asd.", 'only-asd-please-anti-spam' );
+		$netiquette = apply_filters( 'only_asd_please_anti_spam_netiquette', $netiquette );
+		$fields[ 'comment_form_before' ] .= "<p class=\"only-asd-please-anti-spam-netiquette\">$netiquette</p>";
 	}
 	return $fields;
 }
-add_filter( 'comment_form_default_fields', 'no_links_please_anti_spam_form_default_fields' );
+add_filter( 'comment_form_default_fields', 'only_asd_please_anti_spam_form_default_fields' );
 
 /**
  * Unuseful callback fired when the shortcode is used
  */
-function no_links_please_anti_spam_counter() {
-	return get_option( 'no_links_please_anti_spam_count', 0 );
+function only_asd_please_anti_spam_counter() {
+	return get_option( 'only_asd_please_anti_spam_count', 0 );
 }
-add_shortcode( 'no_links_please_anti_spam_counter', 'no_links_please_anti_spam_counter' );
+add_shortcode( 'only_asd_please_anti_spam_counter', 'only_asd_please_anti_spam_counter' );
 
 /**
  * Register the unuseful Dashboard widget
  */
-function no_links_please_anti_spam_dashboard_widget() {
-	wp_add_dashboard_widget( 'no_links_please_anti_spam_dashboard_widget', __( "Anti-spam stats from \"No links, please!\"", 'no-links-please-anti-spam' ), 'no_links_please_anti_spam_dashboard_widget_content' );
+function only_asd_please_anti_spam_dashboard_widget() {
+	wp_add_dashboard_widget( 'only_asd_please_anti_spam_dashboard_widget', __( "Anti-spam stats from \"Only asd, please!\"", 'only-asd-please-anti-spam' ), 'only_asd_please_anti_spam_dashboard_widget_content' );
 }
 
 /**
  * Register the unuseful Dashboard widget content
  */
-function no_links_please_anti_spam_dashboard_widget_content() {
+function only_asd_please_anti_spam_dashboard_widget_content() {
 	echo '<p>';
 	printf(
-		__( "Spammers blocked since activation: %s and counting!", 'no-links-please-anti-spam' ),
-		'<b>' . no_links_please_anti_spam_counter() . '</b>'
+		__( "Spammers blocked since activation: %s and counting!", 'only-asd-please-anti-spam' ),
+		'<b>' . only_asd_please_anti_spam_counter() . '</b>'
 	);
 	echo '</p>';
 }
-add_action( 'wp_dashboard_setup', 'no_links_please_anti_spam_dashboard_widget' );
+add_action( 'wp_dashboard_setup', 'only_asd_please_anti_spam_dashboard_widget' );
 
 // allow shortcodes to be used in widgets
 add_filter( 'widget_text', 'do_shortcode' );
@@ -92,25 +92,25 @@ add_filter( 'widget_text', 'do_shortcode' );
 /**
  * Unuseful callback fired when the plugin is activated
  */
-function no_links_please_anti_spam_activation() {
-	add_option( 'no_links_please_anti_spam_init',  date('U') );
-	add_option( 'no_links_please_anti_spam_count', 0 );
+function only_asd_please_anti_spam_activation() {
+	add_option( 'only_asd_please_anti_spam_init',  date('U') );
+	add_option( 'only_asd_please_anti_spam_count', 0 );
 }
-register_uninstall_hook( __FILE__, 'no_links_please_anti_spam_activation' );
+register_uninstall_hook( __FILE__, 'only_asd_please_anti_spam_activation' );
 
 /**
  * Unuseful callback fired when the plugin is uninstalled
  */
-function no_links_please_anti_spam_uninstall() {
-	delete_option( 'no_links_please_anti_spam_init' );
-	delete_option( 'no_links_please_anti_spam_count' );
+function only_asd_please_anti_spam_uninstall() {
+	delete_option( 'only_asd_please_anti_spam_init' );
+	delete_option( 'only_asd_please_anti_spam_count' );
 }
-register_uninstall_hook( __FILE__, 'no_links_please_anti_spam_uninstall' );
+register_uninstall_hook( __FILE__, 'only_asd_please_anti_spam_uninstall' );
 
 /**
  * Load plugin textdomain
  */
-function no_links_please_anti_spam_load_textdomain() {
-	load_plugin_textdomain( 'no-links-please-anti-spam', false, basename( dirname( __FILE__ ) ) . '/languages' );
+function only_asd_please_anti_spam_load_textdomain() {
+	load_plugin_textdomain( 'only-asd-please-anti-spam', false, basename( dirname( __FILE__ ) ) . '/languages' );
 }
-add_action( 'init', 'no_links_please_anti_spam_load_textdomain' );
+add_action( 'init', 'only_asd_please_anti_spam_load_textdomain' );
